eca086406cf6b96d6e086dd0fa9ecae5b6364f4d
Miles Budden <contact@milesbudden.com>
2021-03-23 17:21:53 +0000

    The directory now works

--- top/views.py
+++ top/views.py
@@ -6,7 +6,11 @@ from django.shortcuts import get_list_or_404, get_object_or_404
 # Create your views here.
 
 def home(request):
-    return HttpResponse("hello")
+    payload = ""
+    bikes = get_list_or_404(Bike)
+    for bike in bikes:
+        payload += '<a href="/bike/' + str(bike.number) + '">' + bike.name + '</a>' + '<br>'
+    return HttpResponse(payload)
 
 def bike(request, number):
     bike = get_object_or_404(Bike, number=number)

ea2e7450f1bf5236766bf622a04d72de13eda1be
Miles Budden <contact@milesbudden.com>
2021-03-23 15:23:25 +0000

    Added run command

--- top/management/commands/pull_data.py
+++ top/management/commands/pull_data.py
@@ -1,20 +1,9 @@
 from django.core.management.base import BaseCommand, CommandError
-# from polls.models import Question as Poll
+from top.models import Bike, Bike_position, Location
 
-# class Command(BaseCommand):
-#     help = 'Closes the specified poll for voting'
+class Command(BaseCommand):
+    help = 'Pulls new data from nextbike'
 
-#     # def add_arguments(self, parser):
-#     #     parser.add_argument('poll_ids', nargs='+', type=int)
-
-#     def handle(self, *args, **options):
-#         for poll_id in options['poll_ids']:
-#             try:
-#                 poll = Poll.objects.get(pk=poll_id)
-#             except Poll.DoesNotExist:
-#                 raise CommandError('Poll "%s" does not exist' % poll_id)
-
-#             poll.opened = False
-#             poll.save()
-
-#             self.stdout.write(self.style.SUCCESS('Successfully closed poll "%s"' % poll_id))
+    def handle(self, *args, **options):
+        self.stdout.write(self.style.SUCCESS('Successfully run the pull command'))
+        self.stdout.write(self.style.WARNING('This has not actually been built yet'))
